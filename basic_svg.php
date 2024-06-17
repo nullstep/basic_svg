687abcc0d73dadad1d0a26d056f70c53d275b236
nullstep <me@nullstep.com>
2024-06-17 02:17:45 +0100

    shortcode fix, updater, other fixes

--- basic_svg.php
+++ basic_svg.php
@@ -2,11 +2,11 @@
 
 /*
  * Plugin Name: basic_svg
- * Plugin URI: https://localhost/plugins
+ * Plugin URI: https://nullstep.com/wp-plugins
  * Description: make svgs available
  * Author: nullstep
- * Author URI: https://localhost
- * Version: 1.0.1
+ * Author URI: https://nullstep.com
+ * Version: 1.0.2
  */
 
 defined('ABSPATH') or die('⎺\_(ツ)_/⎺');
@@ -197,9 +197,9 @@ class _bsMenu {
 			$this->slug,
 			[$this, 'render_admin'],
 			'data:image/svg+xml;base64,' . base64_encode(
-				'<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="500px" height="500px" viewbox="0 0 500 500"><path fill="#a7aaad" d="M250,9.8L42,129.9v240.2l208,120.1l208-120.1V129.9L250,9.8z M407.42,263.66l-44.6-25.75v-53.04L250,119.72 L159.46,172l102.78,59.34l145.18,83.82v25.74L250,431.79L92.58,340.89V236.06l44.6,25.75v53.33L250,380.28l90.5-52.25l-51.24-29.58	l0.15-0.25L92.58,184.55v-25.45L250,68.21l157.42,90.89V263.66z"/></svg>'
+				'<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="500px" height="500px" viewbox="0 0 500 500"><path fill="#a7aaad" d="M250.2,17.4L49,133.5v232.3L250.2,482l201.2-116.2V133.5L250.2,17.4z M371.9,203.2H177.2v23.2h194.6v19.1v29.6 v23.2V347h-48.7H128.4v-48.7h194.7v-23.2H128.4v-48.7h0.1v-23.2v-48.7h48.7h194.7V203.2z"/></svg>'
 			),
-			30
+			3
 		);
 
 		// add config submenu
@@ -443,16 +443,19 @@ function bs_svg_metabox($post) {
 		#bs_meta_box input,
 		#bs_meta_box select,
 		#bs_meta_box textarea,
-		#bs_meta_box span.preview {
+		#bs_meta_box #bs-preview {
 			box-sizing: border-box;
 			display: block;
 			vertical-align: middle;
 			margin-top: 10px;
 			float: none;
 			background: #fff;
+			transition: .3s;
+			border: 1px solid #ccc;
+			border-radius: 4px;
 		}
-		#bs_meta_box span.preview:hover {
-			background: #000;
+		#bs_meta_box #bs-preview:hover {
+			background: #eee;
 		}
 		#bs_meta_box span.desc {
 			display: block;
@@ -461,9 +464,9 @@ function bs_svg_metabox($post) {
 			font-style: italic;
 			font-size: 12px;
 		}
-		#bs_meta_box span.preview img {
-			max-width: 100%;
-			height: 100px;
+		#bs_meta_box #bs-preview svg {
+			max-height: 100px;
+			width: 100%;
 		}
 		#bs_meta_box div.middle {
 			margin-bottom: 10px;
@@ -497,15 +500,16 @@ function bs_svg_metabox($post) {
 		<div class="bottom">
 			<label>Preview:</label>
 			<span class="desc">Preview of this SVG</span>
-			<span class="preview"><img id="bs-preview" src=""></span>
+			<div id="bs-preview"></div>
 		</div>
 	</div>
 	<script>
 		jQuery(document).ready(function($) {
-			var noimg = btoa('<svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50"><style>#no-img path{fill:#3f3f3f}</style><g id="no-img"><path d="M2.3,24.3c0-0.6,0-1.1,0-1.6h0.9l0.1,1h0c0.3-0.6,1-1.1,1.9-1.1c0.8,0,2.1,0.5,2.1,2.5v3.5H6.2v-3.3c0-0.9-0.3-1.7-1.3-1.7c-0.7,0-1.2,0.5-1.4,1.1c0,0.1-0.1,0.3-0.1,0.5v3.5H2.3V24.3z"/><path d="M14.2,25.6c0,2.1-1.5,3.1-2.9,3.1c-1.6,0-2.8-1.2-2.8-3c0-1.9,1.3-3.1,2.9-3.1C13.1,22.6,14.2,23.8,14.2,25.6zM9.6,25.6c0,1.3,0.7,2.2,1.8,2.2c1,0,1.8-0.9,1.8-2.3c0-1-0.5-2.2-1.7-2.2C10.2,23.4,9.6,24.5,9.6,25.6z"/><path d="M19.3,21.1c0,0.4-0.3,0.6-0.7,0.6c-0.4,0-0.6-0.3-0.6-0.6c0-0.4,0.3-0.7,0.7-0.7C19,20.4,19.3,20.7,19.3,21.1zM18.1,28.5v-5.8h1.1v5.8H18.1z"/><path d="M20.9,24.3c0-0.6,0-1.1,0-1.6h0.9l0,0.9h0c0.3-0.6,0.9-1.1,1.8-1.1c0.8,0,1.4,0.5,1.6,1.2h0c0.2-0.3,0.4-0.6,0.6-0.8c0.3-0.3,0.7-0.4,1.3-0.4c0.8,0,1.9,0.5,1.9,2.5v3.4h-1v-3.3c0-1.1-0.4-1.8-1.3-1.8c-0.6,0-1.1,0.4-1.2,1c0,0.1-0.1,0.3-0.1,0.5v3.6h-1V25c0-0.9-0.4-1.6-1.2-1.6c-0.7,0-1.1,0.5-1.3,1.1C22,24.7,22,24.8,22,25v3.5h-1V24.3z"/><path d="M34.1,28.5l-0.1-0.7h0c-0.3,0.5-0.9,0.9-1.8,0.9c-1.2,0-1.8-0.8-1.8-1.7c0-1.4,1.2-2.2,3.5-2.2v-0.1c0-0.5-0.1-1.3-1.3-1.3c-0.5,0-1.1,0.2-1.5,0.4l-0.2-0.7c0.5-0.3,1.2-0.5,1.9-0.5c1.8,0,2.2,1.2,2.2,2.4v2.2c0,0.5,0,1,0.1,1.4H34.1z M34,25.6c-1.2,0-2.5,0.2-2.5,1.3c0,0.7,0.5,1,1,1c0.8,0,1.2-0.5,1.4-1c0-0.1,0.1-0.2,0.1-0.3V25.6z"/><path d="M41.7,22.7c0,0.4,0,0.9,0,1.6v3.4c0,1.3-0.3,2.1-0.8,2.7c-0.6,0.5-1.4,0.7-2.1,0.7c-0.7,0-1.5-0.2-1.9-0.5l0.3-0.8c0.4,0.2,1,0.5,1.7,0.5c1.1,0,1.9-0.6,1.9-2v-0.6h0c-0.3,0.5-0.9,1-1.8,1c-1.4,0-2.5-1.2-2.5-2.8c0-2,1.3-3.1,2.6-3.1c1,0,1.6,0.5,1.8,1h0l0-0.9H41.7z M40.6,25c0-0.2,0-0.3-0.1-0.5c-0.2-0.6-0.7-1.1-1.5-1.1c-1,0-1.7,0.9-1.7,2.2c0,1.1,0.6,2.1,1.7,2.1c0.6,0,1.2-0.4,1.5-1.1c0.1-0.2,0.1-0.4,0.1-0.6V25z"/><path d="M44,25.8c0,1.4,0.9,2,2,2c0.8,0,1.2-0.1,1.6-0.3l0.2,0.8c-0.4,0.2-1,0.4-1.9,0.4c-1.8,0-2.9-1.2-2.9-2.9c0-1.8,1-3.1,2.7-3.1c1.9,0,2.4,1.7,2.4,2.7c0,0.2,0,0.4,0,0.5H44z M47.1,25.1c0-0.7-0.3-1.7-1.5-1.7c-1.1,0-1.5,1-1.6,1.7H47.1z"/></g></svg>');
+			var noimg = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50"><style>#no-img path{fill:#3f3f3f}</style><g id="no-img"><path d="M2.3,24.3c0-0.6,0-1.1,0-1.6h0.9l0.1,1h0c0.3-0.6,1-1.1,1.9-1.1c0.8,0,2.1,0.5,2.1,2.5v3.5H6.2v-3.3c0-0.9-0.3-1.7-1.3-1.7c-0.7,0-1.2,0.5-1.4,1.1c0,0.1-0.1,0.3-0.1,0.5v3.5H2.3V24.3z"/><path d="M14.2,25.6c0,2.1-1.5,3.1-2.9,3.1c-1.6,0-2.8-1.2-2.8-3c0-1.9,1.3-3.1,2.9-3.1C13.1,22.6,14.2,23.8,14.2,25.6zM9.6,25.6c0,1.3,0.7,2.2,1.8,2.2c1,0,1.8-0.9,1.8-2.3c0-1-0.5-2.2-1.7-2.2C10.2,23.4,9.6,24.5,9.6,25.6z"/><path d="M19.3,21.1c0,0.4-0.3,0.6-0.7,0.6c-0.4,0-0.6-0.3-0.6-0.6c0-0.4,0.3-0.7,0.7-0.7C19,20.4,19.3,20.7,19.3,21.1zM18.1,28.5v-5.8h1.1v5.8H18.1z"/><path d="M20.9,24.3c0-0.6,0-1.1,0-1.6h0.9l0,0.9h0c0.3-0.6,0.9-1.1,1.8-1.1c0.8,0,1.4,0.5,1.6,1.2h0c0.2-0.3,0.4-0.6,0.6-0.8c0.3-0.3,0.7-0.4,1.3-0.4c0.8,0,1.9,0.5,1.9,2.5v3.4h-1v-3.3c0-1.1-0.4-1.8-1.3-1.8c-0.6,0-1.1,0.4-1.2,1c0,0.1-0.1,0.3-0.1,0.5v3.6h-1V25c0-0.9-0.4-1.6-1.2-1.6c-0.7,0-1.1,0.5-1.3,1.1C22,24.7,22,24.8,22,25v3.5h-1V24.3z"/><path d="M34.1,28.5l-0.1-0.7h0c-0.3,0.5-0.9,0.9-1.8,0.9c-1.2,0-1.8-0.8-1.8-1.7c0-1.4,1.2-2.2,3.5-2.2v-0.1c0-0.5-0.1-1.3-1.3-1.3c-0.5,0-1.1,0.2-1.5,0.4l-0.2-0.7c0.5-0.3,1.2-0.5,1.9-0.5c1.8,0,2.2,1.2,2.2,2.4v2.2c0,0.5,0,1,0.1,1.4H34.1z M34,25.6c-1.2,0-2.5,0.2-2.5,1.3c0,0.7,0.5,1,1,1c0.8,0,1.2-0.5,1.4-1c0-0.1,0.1-0.2,0.1-0.3V25.6z"/><path d="M41.7,22.7c0,0.4,0,0.9,0,1.6v3.4c0,1.3-0.3,2.1-0.8,2.7c-0.6,0.5-1.4,0.7-2.1,0.7c-0.7,0-1.5-0.2-1.9-0.5l0.3-0.8c0.4,0.2,1,0.5,1.7,0.5c1.1,0,1.9-0.6,1.9-2v-0.6h0c-0.3,0.5-0.9,1-1.8,1c-1.4,0-2.5-1.2-2.5-2.8c0-2,1.3-3.1,2.6-3.1c1,0,1.6,0.5,1.8,1h0l0-0.9H41.7z M40.6,25c0-0.2,0-0.3-0.1-0.5c-0.2-0.6-0.7-1.1-1.5-1.1c-1,0-1.7,0.9-1.7,2.2c0,1.1,0.6,2.1,1.7,2.1c0.6,0,1.2-0.4,1.5-1.1c0.1-0.2,0.1-0.4,0.1-0.6V25z"/><path d="M44,25.8c0,1.4,0.9,2,2,2c0.8,0,1.2-0.1,1.6-0.3l0.2,0.8c-0.4,0.2-1,0.4-1.9,0.4c-1.8,0-2.9-1.2-2.9-2.9c0-1.8,1-3.1,2.7-3.1c1.9,0,2.4,1.7,2.4,2.7c0,0.2,0,0.4,0,0.5H44z M47.1,25.1c0-0.7-0.3-1.7-1.5-1.7c-1.1,0-1.5,1-1.6,1.7H47.1z"/></g></svg>';
 			var preview = function() {
-				var bsf = btoa($('#bs-svg-code').val());
-				$('#bs-preview').attr('src', 'data:image/svg+xml;base64,' + (bsf || noimg));
+				var bsf = $('#bs-svg-code').val();
+				$('#bs-preview').empty().append((bsf || noimg));
+				//$('#bs-preview').attr('src', 'data:image/svg+xml;base64,' + (bsf || noimg));
 			}
 			var markup = $('#bs-svg-code');
 			markup.on('change', function() {
@@ -587,13 +591,15 @@ function bs_set_current_menu($parent_file) {
 //  ████████▀    ▀██████▀   ████████▀     ██████████  
 
 function bs_shortcode($atts = [], $content = null, $tag = '') {
-	$code = '';
+	global $wpdb;
+
 	$a = shortcode_atts([
         'width' => '',
-        'height' => ''
+        'height' => '',
+        'wide' => ''
     ], $atts);
 
-	if (strpos($content, ',') !== FALSE) {
+	if (strpos($content, ',') !== false) {
 		$array = explode(',', $content);
 		$name = $array[rand(0, count($array) - 1)];
 	}
@@ -601,33 +607,34 @@ function bs_shortcode($atts = [], $content = null, $tag = '') {
 		$name = $content;
 	}
 
+	$html = '';
+
 	if ($name) {
-		$svg = get_posts([
-			'post_name' => $name,
-			'post_type'   => 'svg',
-			'post_status' => 'publish',
-			'numberposts' => 1
-		]);
+		$posts = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}posts WHERE `post_title` = ('" . $name . "')");
+		$svg = array_shift($posts);
+		$code = get_post_meta($svg->ID, '_bs-svg_code', true);
+		$width = ($a['width']) ? ' width="' . $a['width'] . '"' : '';
+		$height = ($a['height']) ? ' height="' . $a['height'] . '"' : '';
 
-		$id = $svg[0]->ID;
-		$pid = get_post_meta($id, '_bs-svg_pid', true);
-		$code = get_post_meta($id, '_bs-svg_code', true);
+		$svg = str_replace('<svg', '<svg id="' . sanitize_title($name) . '" ' . $width . $height, $code);
+
+		if ($a['wide'] == 'yes') {
+			if (class_exists('B')) {
+						$html .= '</div>';
+					$html .= '</div>';
+				$html .= '</div>';
+				$html .= $svg;
+				$html .= '<div class="' . B::value('container_class', false) . '">';
+					$html .= '<div class="row">';
+						$html .= '<div class="col-12">';
+			}
+		}
+		else {
+			$html .= $svg;
+		}
 	}
 
-	$width = ($a['width']) ? ' width="' . $a['width'] . '"' : '';
-	$height = ($a['height']) ? ' height="' . $a['height'] . '"' : '';
-
-	return str_replace('<svg', '<svg' . $width . $height, $code);
-}
-
-// some admin styling
-
-function bs_admin_styling() {
-	if (get_current_screen()->post_type == 'svg') {
-		echo '<style>';
-			echo '';
-		echo '</style>';
-	}
+	return $html;
 }
 
 // add admin scripts
@@ -640,25 +647,198 @@ function bs_add_scripts($hook) {
 
 // caller function
 
-function basic_svg($svg) {
-	echo do_shortcode('[svg]' . $svg . '[/svg]');
+function basic_svg($svg, $width = null, $height = null) {
+	$w = ($width) ? ' width="' . $width . '"' : '';
+	$h = ($height) ? ' height="' . $height . '"' : '';
+
+	echo do_shortcode('[svg' . $w . $h . ']' . $svg . '[/svg]');
 }
 
-//     ▄██████▄    ▄██████▄   
-//    ███    ███  ███    ███  
-//    ███    █▀   ███    ███  
-//   ▄███         ███    ███  
-//  ▀▀███ ████▄   ███    ███  
-//    ███    ███  ███    ███  
-//    ███    ███  ███    ███  
-//    ████████▀    ▀██████▀   
+//  ███    █▄      ▄███████▄  ████████▄      ▄████████      ███         ▄████████  
+//  ███    ███    ███    ███  ███   ▀███    ███    ███  ▀█████████▄    ███    ███  
+//  ███    ███    ███    ███  ███    ███    ███    ███     ▀███▀▀██    ███    █▀   
+//  ███    ███    ███    ███  ███    ███    ███    ███      ███   ▀   ▄███▄▄▄      
+//  ███    ███  ▀█████████▀   ███    ███  ▀███████████      ███      ▀▀███▀▀▀      
+//  ███    ███    ███         ███    ███    ███    ███      ███        ███    █▄   
+//  ███    ███    ███         ███   ▄███    ███    ███      ███        ███    ███  
+//  ████████▀    ▄████▀       ████████▀     ███    █▀      ▄████▀      ██████████
+
+if (!class_exists('WPU')) {
+	class WPU {
+		private $file;
+		private $plugin;
+		private $basename;
+		private $active;
+		private $username;
+		private $repository;
+		private $authorize_token;
+		private $github_response;
+
+		private $requires;
+		private $tested;
+
+		public function __construct($file) {
+			$this->file = $file;
+			add_action('admin_init', [$this, 'set_plugin_properties']);
+
+			return $this;
+		}
+
+		public function set_plugin_properties() {
+			$this->plugin = get_plugin_data($this->file);
+			$this->basename = plugin_basename($this->file);
+			$this->active = is_plugin_active($this->basename);
+		}
+
+		public function set_versions($requires, $tested) {
+			$this->requires = $requires;
+			$this->tested = $tested;
+		}
+
+		public function set_username($username) {
+			$this->username = $username;
+		}
+
+		public function set_repository($repository) {
+			$this->repository = $repository;
+		}
+
+		public function authorize($token) {
+			$this->authorize_token = $token;
+		}
+
+		private function get_repository_info() {
+			if (is_null($this->github_response)) {
+				$request_uri = sprintf('https://api.github.com/repos/%s/%s/releases', $this->username, $this->repository);
+
+				$curl = curl_init();
+
+				curl_setopt_array($curl, [
+					CURLOPT_URL => $request_uri,
+					CURLOPT_RETURNTRANSFER => true,
+					CURLOPT_ENCODING => '',
+					CURLOPT_MAXREDIRS => 10,
+					CURLOPT_TIMEOUT => 0,
+					CURLOPT_FOLLOWLOCATION => true,
+					CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+					CURLOPT_CUSTOMREQUEST => 'GET',
+					CURLOPT_HTTPHEADER => [
+						'Authorization: token ' . $this->authorize_token,
+						'User-Agent: WPUpdater/1.0.0'
+					]
+				]);
+
+				$response = curl_exec($curl);
+
+				curl_close($curl);
+
+				$response = json_decode($response, true);
+
+				if (is_array($response)) {
+					$response = current($response);
+				}
+
+				$this->github_response = $response;
+			}
+		}
+
+		public function initialize() {
+			add_filter('pre_set_site_transient_update_plugins', [$this, 'modify_transient'], 10, 1);
+			add_filter('plugins_api', [$this, 'plugin_popup'], 10, 3);
+			add_filter('upgrader_post_install', [$this, 'after_install'], 10, 3);
+		}
+
+		public function modify_transient($transient) {
+			if (property_exists($transient, 'checked')) {
+				if ($checked = $transient->checked) {
+					$this->get_repository_info();
+
+					$out_of_date = version_compare($this->github_response['tag_name'], $checked[$this->basename], 'gt');
+
+					if ($out_of_date) {
+						$new_files = $this->github_response['zipball_url'];
+						$slug = current(explode('/', $this->basename));
+
+						$plugin = [
+							'url' => $this->plugin['PluginURI'],
+							'slug' => $slug,
+							'package' => $new_files,
+							'new_version' => $this->github_response['tag_name']
+						];
+
+						$transient->response[$this->basename] = (object) $plugin;
+					}
+				}
+			}
+
+			return $transient;
+		}
+
+		public function plugin_popup($result, $action, $args) {
+			if ($action !== 'plugin_information') {
+				return false;
+			}
+
+			if (!empty($args->slug)) {
+				if ($args->slug == current(explode('/' , $this->basename))) {
+					$this->get_repository_info();
+
+					$plugin = [
+						'name' => $this->plugin['Name'],
+						'slug' => $this->basename,
+						'requires' => $this->$requires ?? '6.3',
+						'tested' => $this->$tested ?? '6.4.3',
+						'version' => $this->github_response['tag_name'],
+						'author' => $this->plugin['AuthorName'],
+						'author_profile' => $this->plugin['AuthorURI'],
+						'last_updated' => $this->github_response['published_at'],
+						'homepage' => $this->plugin['PluginURI'],
+						'short_description' => $this->plugin['Description'],
+						'sections' => [
+							'Description' => $this->plugin['Description'],
+							'Updates' => $this->github_response['body'],
+						],
+						'download_link' => $this->github_response['zipball_url']
+					];
+
+					return (object) $plugin;
+				}
+			}
+
+
+			return $result;
+		}
+
+		public function after_install($response, $hook_extra, $result) {
+			global $wp_filesystem;
+
+			$install_directory = plugin_dir_path($this->file);
+			$wp_filesystem->move($result['destination'], $install_directory);
+			$result['destination'] = $install_directory;
+
+			if ($this->active) {
+				activate_plugin($this->basename);
+			}
+
+			return $result;
+		}
+	}
+}
+
+//   ▄█   ███▄▄▄▄▄     ▄█       ███      
+//  ███   ███▀▀▀▀██▄  ███   ▀█████████▄  
+//  ███▌  ███    ███  ███▌     ▀███▀▀██  
+//  ███▌  ███    ███  ███▌      ███   ▀  
+//  ███▌  ███    ███  ███▌      ███      
+//  ███   ███    ███  ███       ███      
+//  ███   ███    ███  ███       ███      
+//  █▀     ▀█    █▀   █▀       ▄████▀
 
 define('_BS', _bsSettings::get_settings());
 
 // actions
 
 add_action('init', 'bs_init');
-add_action('admin_head', 'bs_admin_styling');
 add_action('admin_enqueue_scripts', 'bs_add_scripts');
 add_action('add_meta_boxes', 'bs_add_metaboxes');
 add_action('save_post', 'bs_save_postdata');
@@ -676,6 +856,17 @@ add_shortcode('svg', 'bs_shortcode');
 add_action('init', function() {
 	if (is_admin()) {
 		new _bsMenu(_URL_BASIC_SVG);
+
+		// init updater
+
+		if (get_option('auth_key') !== '') {
+			$updater = new WPU(__FILE__);
+			$updater->set_versions('6.4', '6.4.3');
+			$updater->set_username('nullstep');
+			$updater->set_repository('basic_svg');
+			$updater->authorize(get_option('auth_key'));
+			$updater->initialize();
+		}
 	}
 });
 
@@ -684,14 +875,5 @@ add_action('rest_api_init', function() {
 	$api = new _bsAPI();
 	$api->add_routes();
 });
-
-//     ▄█    █▄        ▄████████   ▄█           ▄███████▄  
-//    ███    ███      ███    ███  ███          ███    ███  
-//    ███    ███      ███    █▀   ███          ███    ███  
-//   ▄███▄▄▄▄███▄▄   ▄███▄▄▄      ███          ███    ███  
-//  ▀▀███▀▀▀▀███▀   ▀▀███▀▀▀      ███        ▀█████████▀   
-//    ███    ███      ███    █▄   ███          ███         
-//    ███    ███      ███    ███  ███▌    ▄    ███         
-//    ███    █▀       ██████████  █████▄▄██   ▄████▀
 
 // eof
